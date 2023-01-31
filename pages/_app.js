c8813c05fa422c5cc767feb73a21a3684d1ae862
Odd Egil Hegge Selnes <post@odde.org>
2023-01-31 11:04:09 +0100

    Update _app.js
    Implementet google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,31 @@ config.autoAddCss = false;
 
 import '../styles/globals.css'
 import React from "react";
-
+import Script from "next/script";
 
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  
+  return (
+    <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+      __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-VHQ0TTFV2S', {
+      page_path: window.location.pathname,
+      });
+      `,
+    }}
+    />
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
